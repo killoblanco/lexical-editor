f7e58839055d6ef6570a9f651d680d79778738bd
Kamilo Vasquez <killo.blanco@gmail.com>
2025-11-04 22:58:37 -0500

    Replace Wysiwyg component with LexicalEditor in LandingPage

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,14 +1,14 @@
+import { LexicalEditor } from "@/components/lexical/lexical"
+import { Toolbar } from "@/components/lexical/plugins/toolbar"
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin"
-import { Toolbar } from "@/components/wysiwyg/plugins/toolbar"
-import { Wysiwyg } from "@/components/wysiwyg/wysiwyg"
 
 export const LandingPage = () => {
   return (
     <div className="w-full max-w-5xl mx-auto min-h-screen">
-      <Wysiwyg classNames={{ placeholder: "top-15" }}>
+      <LexicalEditor classNames={{ placeholder: "top-15" }}>
         <Toolbar />
         <HistoryPlugin />
-      </Wysiwyg>
+      </LexicalEditor>
     </div>
   )
 }
