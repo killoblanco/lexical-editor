ab778c2c4824fa1873322008dcb32133bf5f9d5c
Kamilo Vasquez <killo.blanco@gmail.com>
2025-11-05 11:28:14 -0500

    Add TextAlignControls to LandingPage for text alignment options

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -2,6 +2,7 @@ import { ChevronRightIcon, GithubIcon } from "lucide-react"
 import { Link } from "react-router"
 import { LexicalEditor } from "@/components/lexical/editor"
 import { HistoryControls } from "@/components/lexical/toolbar/history-ctrl"
+import { TextAlignControls } from "@/components/lexical/toolbar/text-align-ctrl"
 import { TextFormatControls } from "@/components/lexical/toolbar/text-format-ctrl"
 import { Toolbar } from "@/components/lexical/toolbar/toolbar"
 import { Button } from "@/components/ui/button"
@@ -28,6 +29,7 @@ export const LandingPage = () => {
         <Toolbar>
           <HistoryControls />
           <TextFormatControls />
+          <TextAlignControls />
         </Toolbar>
       </LexicalEditor>
       <div className="flex justify-end">
