fcfd38fb6bee322e9c99cbfe5df2dbb80a026e45
Kamilo Vasquez <killo.blanco@gmail.com>
2025-11-04 21:14:25 -0500

    Remove additional props from Wysiwyg component in LandingPage

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -3,7 +3,7 @@ import { Wysiwyg } from "@/components/wysiwyg/wysiwyg"
 export const LandingPage = () => {
   return (
     <div className="w-full max-w-5xl mx-auto min-h-screen">
-      <Wysiwyg namespace="MyEditor" theme={{}} onError={console.error} />
+      <Wysiwyg />
     </div>
   )
 }
