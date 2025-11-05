287f225abbf69c318ce173f4168514c7927fd18d
Kamilo Vasquez <killo.blanco@gmail.com>
2025-11-04 22:30:07 -0500

    Enhance LandingPage component by adding Wysiwyg configuration with Toolbar and HistoryPlugin

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,9 +1,14 @@
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin"
+import { Toolbar } from "@/components/wysiwyg/plugins/toolbar"
 import { Wysiwyg } from "@/components/wysiwyg/wysiwyg"
 
 export const LandingPage = () => {
   return (
     <div className="w-full max-w-5xl mx-auto min-h-screen">
-      <Wysiwyg />
+      <Wysiwyg classNames={{ placeholder: "top-15" }}>
+        <Toolbar />
+        <HistoryPlugin />
+      </Wysiwyg>
     </div>
   )
 }
