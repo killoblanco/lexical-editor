b55eb1c4384e2fa037920c5f7762b6067d503819
Kamilo Vasquez <killo.blanco@gmail.com>
2025-11-05 00:13:32 -0500

    Add IntroductionPage route to the router for navigation

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 import { PageLayout } from "@/components/layout"
 import { LandingPage } from "@/pages/landing"
+import { IntroductionPage } from "./pages/introduction"
 
 export const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route element={<PageLayout />}>
         <Route index={true} element={<LandingPage />} />
+        <Route path="introduction" element={<IntroductionPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
