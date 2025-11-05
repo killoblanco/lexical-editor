2bcc763155036d39702e244b535f51244445db12
Kamilo Vasquez <killo.blanco@gmail.com>
2025-11-05 14:20:29 -0500

    Add route for InstallationPage in the router

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 import { PageLayout } from "@/components/app/layout"
 import { LandingPage } from "@/pages/landing"
+import { InstallationPage } from "./pages/installation"
 import { IntroductionPage } from "./pages/introduction"
 
 export const Router = () => (
@@ -9,6 +10,7 @@ export const Router = () => (
       <Route element={<PageLayout />}>
         <Route index={true} element={<LandingPage />} />
         <Route path="introduction" element={<IntroductionPage />} />
+        <Route path="installation" element={<InstallationPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
