853532e46c848837d5f301c59dae58fc0640c733
Kamilo Vasquez <killo.blanco@gmail.com>
2025-11-04 21:12:08 -0500

    Replace landing page content with Wysiwyg editor component

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,3 +1,9 @@
+import { Wysiwyg } from "@/components/wysiwyg/wysiwyg"
+
 export const LandingPage = () => {
-  return <p>Landing Page</p>
+  return (
+    <div className="w-full max-w-5xl mx-auto min-h-screen">
+      <Wysiwyg namespace="MyEditor" theme={{}} onError={console.error} />
+    </div>
+  )
 }
