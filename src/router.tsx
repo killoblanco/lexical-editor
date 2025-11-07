9b08ea93589c9d63e04546e0e3273fc039f912ed
Kamilo Vasquez <killo.blanco@gmail.com>
2025-11-06 19:50:29 -0500

    Add routes for LexicalEditorPage and ToolbarPage in Router

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 import { PageLayout } from "@/components/app/layout"
 import { LandingPage } from "@/pages/landing"
+import { LexicalEditorPage } from "./pages/components/lexical-editor"
+import { ToolbarPage } from "./pages/components/toolbar"
 import { InstallationPage } from "./pages/installation"
 import { IntroductionPage } from "./pages/introduction"
 
@@ -11,6 +13,8 @@ export const Router = () => (
         <Route index={true} element={<LandingPage />} />
         <Route path="introduction" element={<IntroductionPage />} />
         <Route path="installation" element={<InstallationPage />} />
+        <Route path="lexical-editor" element={<LexicalEditorPage />} />
+        <Route path="toolbar" element={<ToolbarPage />} />
       </Route>
     </Routes>
   </BrowserRouter>
